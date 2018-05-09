2e43297d00aafd9acc7bcd29d2a8de12870498e2
Estudiante <Estudiante@10.203.1.224>
2018-05-09 16:01:56 -0500

    Step3

---
+++ src/builderpattern/Burger.java
@@ -0,0 +1,16 @@
+package builderpattern;
+
+public abstract class Burger implements Item {
+    
+    @Override
+    public String name(){
+        return "Burger cheese";
+    }
+    
+    @Override
+    public Packing packing(){
+        return new Wrapper();
+    }
+    @Override
+    public abstract float price();
+}
